b3264bbb6960ab7515abeb07ea94f388b93ab118
Otabek Rakhimov <rahimovmail@gmail.com>
2023-04-26 18:05:21 -0700

    Create basic server functions

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-// import ReactDOM from 'react-dom/client';
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Listings } from "./sections/Listings";
+// import { render } from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
-render(<div>hello world</div>, document.getElementById("root"));
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
+// render(
+//   <Listings title="Tiny-Places Listings" />,
+//   document.getElementById("root")
 // );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Listings title="Tiny-Places Listings" />,
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
