94da68e0ecf2508204ed87b7b377351c12cedc36
Otabek Rakhimov <rahimovmail@gmail.com>
2023-04-30 17:47:24 -0700

    Add some basic react hook functions

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { server } from "../../lib/api";
 import {
   DeleteListingData,
   DeleteListingVariables,
+  Listing,
   ListingsData,
 } from "./types";
 
@@ -22,25 +23,52 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `mutation DeleteListing ($id: ID!) {
+  deleteListing(id: $id){
+    id
+  }
+}`;
+
 interface Props {
   title: string;
 }
 
 export const Listings = ({ title }: Props) => {
+  const [listings, setListings] = useState<Listing[] | null>(null);
+
   const fetchListings = async () => {
     const { data } = await server.fetch<ListingsData>({
       query: LISTINGS,
     });
-    console.log(data); // check the console to see the listings data from our GraphQL Request!
+    setListings(data.listings);
   };
 
-  const deleteListing = async () => {};
+  const deleteListing = async (id: string) => {
+    await server.fetch<DeleteListingData, DeleteListingVariables>({
+      query: DELETE_LISTING,
+      variables: { id },
+    });
+    fetchListings();
+  };
+
+  const listingsList = listings ? (
+    <ul>
+      {listings.map((listing) => {
+        return (
+          <li key={listing.id}>
+            {listing.title}
+            <button onClick={() => deleteListing(listing.id)}>Delete</button>
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
 
   return (
     <div>
       <h2>{title}</h2>
+      {listingsList}
       <button onClick={fetchListings}>Query Listings!</button>
-      <button onClick={deleteListing}>Delete a listing!</button>
     </div>
   );
 };
