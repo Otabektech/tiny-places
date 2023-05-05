5f4565577f6d03dbd8f94bbf8f44f2a21d4530b4
Otabek Rakhimov <rahimovmail@gmail.com>
2023-05-04 17:58:05 -0700

    Add some contents to UseQuery and useMutation api

--- client/src/lib/api/useMutation.ts
+++ client/src/lib/api/useMutation.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { server } from "./server";
 
 interface State<TData> {
@@ -7,14 +7,42 @@ interface State<TData> {
   error: boolean;
 }
 
+type Action<TData> =
+  | { type: "FETCH" }
+  | { type: "FETCH_SUCCESS"; payload: TData }
+  | { type: "FETCH_ERROR" };
+
 type MutationTuple<TData, TVariables> = [
   (variables?: TVariables | undefined) => Promise<void>,
   State<TData>
 ];
 
+const reducer =
+  <TData>() =>
+  (state: State<TData>, action: Action<TData>): State<TData> => {
+    switch (action.type) {
+      case "FETCH":
+        return { ...state, loading: true };
+      case "FETCH_SUCCESS":
+        return { data: action.payload, loading: false, error: false };
+      case "FETCH_ERROR":
+        return { ...state, loading: false, error: true };
+      default:
+        throw new Error();
+    }
+  };
+
 export const useMutation = <TData = any, TVariables = any>(
   query: string
 ): MutationTuple<TData, TVariables> => {
+  const fetchReducer = reducer<TData>();
+
+  const [state, dispatch] = useReducer(fetchReducer, {
+    data: null,
+    loading: false,
+    error: false,
+  });
+
   const [state, setState] = useState<State<TData>>({
     data: null,
     loading: false,
