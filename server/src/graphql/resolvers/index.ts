1b8e272de9f1abf7c69b02404db1b131a2ad8639
Otabek Rakhimov <rahimovmail@gmail.com>
2023-06-10 17:05:08 -0700

    Create google api basic functionality

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -2,11 +2,11 @@ import merge from "lodash.merge";
 import { bookingResolvers } from "./Booking";
 import { listingResolvers } from "./Listing";
 import { userResolvers } from "./User";
-import { viewerResolver } from "./Viewer";
+import { viewerResolvers } from "./Viewer";
 
 export const resolvers = merge(
   bookingResolvers,
   listingResolvers,
   userResolvers,
-  viewerResolver
+  viewerResolvers
 );
